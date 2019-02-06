dec78c3056fda5c5f86cbea80bab1bd84b02b146
Jose Ramirez <joseramirez2@csumb.edu>
2019-02-06 22:33:27 +0000

    heroku link fix

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <body>
         <a href= "/practice/p1">Practice1</a>
         <br>
-        <a href= "/assignments">Assignments</a>
+        <a href= "/assignments/hw1">Assignment 1</a>
         <br>
-        <a href= "/labs">Labs</a>
+        <a href= "/labs/lab1-portfolio">Lab 1</a>
     </body>
 </html>
