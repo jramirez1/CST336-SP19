c4f45f64958f7f607e03d9c0d5c40e2308ff5ca0
Jose Ramirez <joseramirez2@csumb.edu>
2019-02-20 23:39:40 +0000

    adding route to index.php

--- index.php
+++ index.php
@@ -6,12 +6,20 @@
     <body>
         <a href= "/practice/p1">Practice1</a>
         <br>
+        HW
         <a href= "/assignments/hw1">Assignment 1</a>
+        <br>
+        <a href= "/assignments/hw2">Project 2 Forms</a>
+        <br>
+        
+        LABS
         <br>
         <a href= "/labs/lab1-portfolio">Lab 1</a>
         <br>
         <a href= "/labs/lab2-guessNumber">Lab 2 Number Guessing</a>
         <br>
-        <a href= "/assignments/hw2">Project 2 Forms</a>
+        <a href= "/labs/lab3-quiz">Lab 3 Quiz</a>
+        <br>
+        
     </body>
 </html>
