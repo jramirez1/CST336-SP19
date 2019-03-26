3160a1b09b765b21d4133dadf19f804d32d1be8f
Jose Ramirez <joseramirez2@csumb.edu>
2019-03-26 00:04:56 +0000

    heroku db migration

--- labs/lab7-otterMart/dbConnection.php
+++ labs/lab7-otterMart/dbConnection.php
@@ -6,6 +6,18 @@ function get_database_connection($dbname = 'ottermart'){
     $username = 'root';
     $password = '';
     
+
+    
+     //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("mysql://bbc314c67abb43:b336bb73@us-cdbr-iron-east-03.cleardb.net/heroku_cef7aa65e0d770c?reconnect=true"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
+    
     //creates database connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
